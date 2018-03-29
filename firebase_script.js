7330bff05c940281c5cdb45a72643de128890d8c
Mary Chew <jiayi1172@gmail.com>
2018-03-29 19:49:08 +0800

    Add files via upload

---
+++ firebase_script.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCmQl9j21ObPxu2dpsi7X6dYqaPrmmZPb4",
+        authDomain: "todolist-d831e.firebaseapp.com",
+        databaseURL: "https://todolist-d831e.firebaseio.com",
+        projectId: "todolist-d831e",
+        storageBucket: "todolist-d831e.appspot.com",
+        messagingSenderId: "431864077565"
+    };
+    firebase.initializeApp(config);
